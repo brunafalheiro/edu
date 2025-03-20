684b81640ca3dfc614a72769a16021b8743aa78f
brunafalheiro <brunafalheiro@yahoo.com.br>
2025-03-19 22:38:33 -0300

    Import button

--- src/ui/HelloWorld.vue
+++ src/ui/HelloWorld.vue
@@ -1,7 +1,12 @@
 <template>
-  Hello World
+  <div>
+    <p>Hello World</p>
+    <Button>Button</Button>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import Button from '@components/ui/button/Button.vue';
+</script>
 
 <style setup></style>
