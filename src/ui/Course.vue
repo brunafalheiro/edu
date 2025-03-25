956644bebff6e0f74acf539fdc9f7bca2c62a330
brunafalheiro <brunafalheiro@yahoo.com.br>
2025-03-25 10:58:57 -0300

    Justify text

--- src/ui/Course.vue
+++ src/ui/Course.vue
@@ -4,7 +4,7 @@
       <div class="w-full max-w-[860px] p-12 pt-24">
         <Button @click="goBack">Voltar</Button>
         <p class="text-xl font-black mb-6 mt-12">Aula</p>
-        <p class="mb-12">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam placerat eget lacus et luctus. Nam auctor, felis vel facilisis tincidunt, lectus enim euismod nunc, nec pretium lorem lorem vel tortor. Integer arcu lorem, gravida eu euismod quis, congue at libero. Nullam facilisis, lectus vel efficitur scelerisque, nisl nibh sodales urna, quis imperdiet ligula neque et metus. Fusce quis leo et mi molestie eleifend ac at enim. Donec et metus quis augue imperdiet accumsan. Donec vel nisi a augue condimentum fringilla. Vestibulum ut orci finibus, porttitor mauris ut, tincidunt augue. Morbi vulputate massa non felis faucibus maximus. Curabitur feugiat, nibh eget cursus hendrerit, orci leo facilisis massa, eu maximus odio mi sit amet lorem. Sed faucibus, dolor eu scelerisque ullamcorper, mauris purus blandit arcu, ac egestas neque tellus sed nunc.
+        <p class="text-justify mb-12">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam placerat eget lacus et luctus. Nam auctor, felis vel facilisis tincidunt, lectus enim euismod nunc, nec pretium lorem lorem vel tortor. Integer arcu lorem, gravida eu euismod quis, congue at libero. Nullam facilisis, lectus vel efficitur scelerisque, nisl nibh sodales urna, quis imperdiet ligula neque et metus. Fusce quis leo et mi molestie eleifend ac at enim. Donec et metus quis augue imperdiet accumsan. Donec vel nisi a augue condimentum fringilla. Vestibulum ut orci finibus, porttitor mauris ut, tincidunt augue. Morbi vulputate massa non felis faucibus maximus. Curabitur feugiat, nibh eget cursus hendrerit, orci leo facilisis massa, eu maximus odio mi sit amet lorem. Sed faucibus, dolor eu scelerisque ullamcorper, mauris purus blandit arcu, ac egestas neque tellus sed nunc.
           Ut nec convallis urna, sed accumsan ex. Suspendisse cursus, libero sed rhoncus imperdiet, ipsum ipsum vehicula arcu, viverra pretium purus nunc sit amet libero. Aliquam quis quam ac leo tincidunt convallis vel sed est. Suspendisse dictum ex dui. Quisque suscipit eros tellus, id molestie sapien aliquam sed. Praesent faucibus mattis suscipit. Sed eu tincidunt dui, nec maximus dolor. Ut in ultricies nibh. Donec consectetur fermentum enim eu dictum. Quisque vitae diam eu purus congue tempus vitae at nisi. Cras lobortis dolor sit amet elit pretium, quis vulputate lacus sollicitudin. Nulla accumsan at nisl sit amet laoreet.
 
           Fusce pretium ante massa, nec facilisis eros blandit quis. Vestibulum egestas turpis ornare augue semper, vitae interdum nibh accumsan. In hac habitasse platea dictumst. Duis lacinia purus viverra felis fringilla, efficitur molestie ante gravida. Cras dapibus risus metus, vel vehicula justo condimentum non. Maecenas pharetra turpis nisi, et consequat elit iaculis ullamcorper. Morbi ut dui mauris. Etiam sed enim et lacus euismod fermentum sed non nisi. Aenean ac egestas lorem, a posuere lectus. Cras tempor id dui vitae luctus. Vestibulum fermentum libero non mauris egestas, at rutrum felis tempus. Sed rutrum non tortor in commodo. Donec mollis dolor orci, sed malesuada neque sodales sit amet. Phasellus vestibulum, metus eu lobortis mollis, justo tortor gravida lectus, sed fermentum elit massa eu est. Curabitur neque lorem, blandit quis sapien eu, vehicula vestibulum risus.
@@ -49,8 +49,7 @@
 
   const router = useRouter();
   const courseId = router.currentRoute.value.params.id;
-  console.log(courseId);
-
+  
   const course = courses.find(course => course.id === courseId);
   const courseName = course.name;
   const classes = course.classes.map((cls) => {
