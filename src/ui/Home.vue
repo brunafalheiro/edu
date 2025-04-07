1a320befda6cdb15c1981deaced84ec8970f01df
brunafalheiro <brunafalheiro@yahoo.com.br>
2025-04-07 15:44:06 -0300

    Refactor Home.vue: update course completion display logic and integrate carousel for ongoing courses

--- src/ui/Home.vue
+++ src/ui/Home.vue
@@ -6,54 +6,55 @@
           <div
             class="h-24 w-full bg-white rounded-lg flex items-center p-6 mr-4"
           >
-            <p class="text-5xl font-black mr-4">{{ completedCourses }}</p>
+            <p class="text-5xl font-black mr-4">{{ completedCoursesAmount }}</p>
             <p class="text-sm font-medium">Cursos completados</p>
           </div>
           <div class="h-24 w-full bg-white rounded-lg flex items-center p-6">
-            <p class="text-5xl font-black mr-4">{{ ongoingCourses }}</p>
+            <p class="text-5xl font-black mr-4">{{ ongoingCoursesAmount }}</p>
             <p class="text-sm font-medium">Cursos em andamento</p>
           </div>
         </div>
       </div>
 
-      <div class="flex items-center mb-12">
-        <div
-          class="h-[60px] w-full flex justify-between items-center bg-gray-100 rounded-lg px-4 py-2"
-        >
-          <div class="flex items-center justify-center">
-            <div
-              class="flex items-center justify-center rounded-sm h-11 w-11 bg-neutral-50 mr-4"
-            >
-              <img :src="getIcon('iconBinaryTree')" alt="" />
-            </div>
-            <p class="font-bold">Árvores Binárias</p>
-          </div>
-          <Button class="h-8" @click="">Continuar</Button>
-        </div>
-
-        <div class="flex items-center ml-6">
-          <Button
-            variant="outline"
-            size="icon"
-            class="w-12 h-12 rounded-full border-gray-600 mr-3"
-          >
-            <i class="pi pi-angle-left"></i>
-          </Button>
-          <Button
-            variant="outline"
-            size="icon"
-            class="w-12 h-12 rounded-full border-gray-600"
-          >
-            <i class="pi pi-angle-right"></i>
-          </Button>
-        </div>
+      <div v-if="ongoingCourses?.length" class="w-full mb-12">
+        <Carousel>
+          <CarouselPrevious />
+          <CarouselContent>
+            <CarouselItem v-for="course in ongoingCourses">
+              <div
+                class="h-16 w-full flex justify-between items-center bg-gray-100 rounded-lg px-4 py-2"
+              >
+                <div class="flex items-center justify-center">
+                  <div
+                    class="flex items-center justify-center rounded-sm h-11 w-11 bg-neutral-50 mr-4"
+                  >
+                    <img :src="getIcon(course.icon)" alt="" />
+                  </div>
+                  <p class="font-bold">{{ course.name }}</p>
+                </div>
+                <Button
+                  class="h-8"
+                  @click="
+                    goToCourse(
+                      course.id,
+                      course.currentClass,
+                      course.currentTopic
+                    )
+                  "
+                  >Continuar</Button
+                >
+              </div>
+            </CarouselItem>
+          </CarouselContent>
+          <CarouselNext />
+        </Carousel>
       </div>
 
       <p class="text-xl font-black mb-4">Cursos</p>
       <div
         v-for="course in courses"
         key="{{ course.id }}"
-        class="h-[60px] w-full flex justify-between items-center bg-gray-100 rounded-lg px-4 py-2 mb-12"
+        class="h-16 w-full flex justify-between items-center bg-gray-100 rounded-lg px-4 py-2 mb-12"
       >
         <div class="flex items-center justify-center">
           <div
@@ -77,7 +78,7 @@
       <p class="text-xl font-black mb-4">Ferramentas</p>
       <div
         @click="goToBinaryTreeSimulator"
-        class="h-[60px] w-full flex justify-between items-center bg-gray-100 rounded-lg pl-4 pr-6 py-2 mb-3 cursor-pointer group"
+        class="h-16 w-full flex justify-between items-center bg-gray-100 rounded-lg pl-4 pr-6 py-2 mb-3 cursor-pointer group"
       >
         <div class="flex items-center justify-center">
           <div
@@ -100,31 +101,60 @@ import { useRouter } from "vue-router";
 import Button from "@components/ui/button/Button.vue";
 import courses from "@/courses.json";
 import { ref } from "vue";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
-const completedCourses = ref(0);
-const ongoingCourses = ref(0);
+const completedCoursesAmount = ref(0);
+const ongoingCoursesAmount = ref(0);
+const ongoingCourses = ref([]);
 
 const router = useRouter();
 const goToCourseInfo = (courseId) => router.push(`/course/${courseId}/info`);
+const goToCourse = (courseId, classId, topicId) =>
+  router.push(`/course/${courseId}/${classId}/${topicId}`);
 const goToBinaryTreeSimulator = () => router.push("/binary-tree-simulator");
 
 const icons = import.meta.glob("@/assets/icons/*.svg", { eager: true });
 const getIcon = (iconName) =>
   icons[`/src/assets/icons/${iconName}.svg`]?.default || "";
 
-const updateCourseStatus = async () => {
+const loadCoursesData = async () => {
   const progress = (await window.store.get("progress")) ?? {};
+  const courseProgress = Object.entries(progress);
 
-  let totalCompleted = 0;
-  let totalOngoing = 0;
+  let completed = 0;
+  let ongoing = 0;
 
-  for (const course of Object.values(progress)) {
-    course.completed ? totalCompleted++ : totalOngoing++;
-  }
+  courseProgress.forEach(([_, data]) => {
+    data.completed ? completed++ : ongoing++;
+  });
 
-  completedCourses.value = totalCompleted;
-  ongoingCourses.value = totalOngoing;
+  completedCoursesAmount.value = completed;
+  ongoingCoursesAmount.value = ongoing;
+
+  ongoingCourses.value = courseProgress
+    .filter(([_, data]) => !data.completed)
+    .map(([courseId, data]) => {
+      const course = courses.find((c) => c.id === courseId);
+      if (!course) return null;
+
+      const { currentTopic, currentClass } = data;
+
+      return {
+        id: course.id,
+        name: course.name,
+        icon: course.icon,
+        currentTopic,
+        currentClass,
+      };
+    })
+    .filter(Boolean);
 };
 
-updateCourseStatus();
+loadCoursesData();
 </script>
