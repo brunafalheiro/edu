db9a13cc760d9f5d13520fb45071e24400ffd8d9
brunafalheiro <brunafalheiro@yahoo.com.br>
2025-04-27 00:10:54 -0300

    add borders and remove background color

--- src/components/ui/CourseStats.vue
+++ src/components/ui/CourseStats.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="w-full h-[180px] bg-gray-100 rounded-lg relative p-6 mb-12">
+  <div class="w-full h-[180px] rounded-lg border border-black relative p-6 mb-12">
     <div class="max-w-md w-full flex absolute right-6 bottom-6">
-      <div class="h-24 w-full bg-white rounded-lg flex items-center p-6 mr-4">
+      <div class="h-24 w-full border border-black rounded-lg flex items-center p-6 mr-4">
         <p class="text-5xl font-black mr-4">{{ completedCourses }}</p>
         <p class="text-sm font-medium">Cursos finalizados</p>
       </div>
-      <div class="h-24 w-full bg-white rounded-lg flex items-center p-6">
+      <div class="h-24 w-full border border-black rounded-lg flex items-center p-6">
         <p class="text-5xl font-black mr-4">{{ ongoingCourses }}</p>
         <p class="text-sm font-medium">Cursos em andamento</p>
       </div>
