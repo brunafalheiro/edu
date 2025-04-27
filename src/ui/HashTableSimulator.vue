e6b8098be4af9749373d04e3997930f3157c2976
brunafalheiro <brunafalheiro@yahoo.com.br>
2025-04-27 00:31:43 -0300

    include BackButton and improve layout structure

--- src/ui/HashTableSimulator.vue
+++ src/ui/HashTableSimulator.vue
@@ -1,6 +1,20 @@
 <template>
-  Hash Simulator
+  <div class="w-full min-h-screen">
+    <div class="w-full p-6 pt-20 mx-auto" style="height: calc(100vh - 120px)">
+      <BackButton class="mb-6" text="Simulador de Hash Tables" :backFunction="goBack" />
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+  import BackButton from "@/components/ui/BackButton.vue";
+  import { useRouter } from "vue-router";
+
+  const router = useRouter();
+  const goBack = () => router.push("/");
+</script>
+
+<style scoped lang="scss">
+</style>
+
 
