3c4628b79fedc9833a7a4d30a4681bc106637288
brunafalheiro <brunafalheiro@yahoo.com.br>
2025-05-06 21:24:26 -0300

    Improve button ui

--- src/components/ui/BackButton.vue
+++ src/components/ui/BackButton.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="flex items-center">
-    <i
-      class="pi pi-angle-left text-2xl mr-4 mt-1 cursor-pointer transition-transform duration-300 hover:-translate-x-2"
+    <div
+      class="w-8 h-8 bg-white border-2 shadow-[4px_4px_0_0_#000] border-black rounded-full flex items-center justify-center mr-5 cursor-pointer transition-transform duration-300 hover:-translate-x-2"
       @click="backFunction"
-    ></i>
-    <p v-if="text" class="text-2xl font-black">{{ text }}</p>
+    >
+      <i class="pi pi-angle-left text-xl mr-0.5 text-black" />
+    </div>
+    <p v-if="text" class="text-2xl font-black mt-1">{{ text }}</p>
   </div>
 </template>
 
 <script setup>
-  import { defineProps } from 'vue';
-
   defineProps({ text: String, backFunction: Function });
 </script>
